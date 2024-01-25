7be5e8c9b6b7f5d008476a705ea256e058c89ef8
SeanChangX <scx@gapp.nthu.edu.tw>
2024-01-25 21:54:54 +0800

    Change conky_setup.sh behavior

--- conky_setup.sh
+++ conky_setup.sh
@@ -2,3 +2,5 @@
 
 echo "Copying conky config files to ~/.config/conky/"
 cp -r .config/conky ~/.config/
+echo "Copying autostart config files to ~/.config/autostart/"
+cp -r .config/autostart ~/.config/
