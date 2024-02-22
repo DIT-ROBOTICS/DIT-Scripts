58ee186064ec1268f0cf4053cdc3b466d48b5e08
SeanChangX <scx@gapp.nthu.edu.tw>
2024-02-22 23:28:21 +0800

    Change conky_setup.sh running dir

--- conky_setup.sh
+++ conky_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Copying conky config files to ~/.config/conky/"
-cp -r .config/conky ~/.config/
+cp -r .config/conky /home/ditrobotics/.config/
 echo "Copying autostart config files to ~/.config/autostart/"
-cp -r .config/autostart ~/.config/
+cp -r .config/autostart /home/ditrobotics/.config/
