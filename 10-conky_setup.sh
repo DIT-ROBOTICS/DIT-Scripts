07061cf7c6c3e797710efb316ec89b5909c73336
SeanChangX <scx@gapp.nthu.edu.tw>
2024-03-25 22:18:42 +0800

    Update DIT-2024-12 STM SN number

--- 10-conky_setup.sh
+++ 10-conky_setup.sh
@@ -6,3 +6,5 @@ echo "Copying autostart config files to ~/.config/autostart/"
 cp -r .config/autostart /home/ditrobotics/.config/
 echo "Copying udev rules files to /etc/udev/rules.d/"
 cp -r udev/. /etc/udev/rules.d/.
+
+udevadm control --reload-rules && udevadm trigger
