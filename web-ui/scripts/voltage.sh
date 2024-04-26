dfb332fd3f7d0d8c737cdde9f220c78d17b86e34
SeanChangX <scx@gapp.nthu.edu.tw>
2024-04-27 05:23:35 +0800

    Update web page voltage file path

--- web-ui/scripts/voltage.sh
+++ web-ui/scripts/voltage.sh
@@ -2,6 +2,6 @@
 
 while true; do
     output=$(/home/ditrobotics/.config/conky/conky-esp-config/conky_esp32.sh)
-    echo $output > /home/ditrobotics/DIT-Scripts/web-ui/data/voltage.cache 
+    echo $output > /home/share/voltage.cache 
     sleep 1
 done
