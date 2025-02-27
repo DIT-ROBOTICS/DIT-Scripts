2304f672aecec4d1be13c8e9b570f8a5375369db
SeanChangX <scx@gapp.nthu.edu.tw>
2025-02-28 03:42:19 +0800

    Update entrypoint script and Docker Compose: modify entrypoint command and add ROS setup for noetic and humble distributions

--- web/docker/entrypoint.sh
+++ web/docker/entrypoint.sh
@@ -5,8 +5,10 @@ export ROS_DOMAIN_ID=${HOSTNAME##*-}
 echo "ROS_DOMAIN_ID set to: $ROS_DOMAIN_ID"
 
 if [ "$ROS_DISTRO" = 'noetic' ]; then
+    source /opt/ros/noetic/setup.bash
     roslaunch foxglove_bridge foxglove_bridge.launch;
 elif [ "$ROS_DISTRO" = 'humble' ]; then
+    source /opt/ros/humble/setup.bash
     ros2 launch foxglove_bridge foxglove_bridge_launch.xml &
     ros2 launch teleop_twist_joy teleop-launch.py joy_config:='xbox' &
     ros2 run image_tools cam2image --ros-args --log-level WARN -p video_device:=/dev/video0;
