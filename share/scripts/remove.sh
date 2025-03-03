311ea5a00fd148936fb587df992cd1c030527a58
SeanChangX <scx@gapp.nthu.edu.tw>
2025-03-04 02:31:49 +0800

    Add desktop entries and scripts for launching DIT applications with tmux sessions

--- share/scripts/remove.sh
+++ share/scripts/remove.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Run a new session
+tmux new-session -d -s eurobot-kill-script
+
+# Remove localization
+tmux send-keys -t 0 "echo 'ditrobotics' | sudo -S docker compose -p communication -f /home/localization/Eurobot-2025-Localization/docker/testBot/OdomComm/docker/yellow-compose.yml down" C-m
+tmux send-keys -t 0 "echo 'ditrobotics' | sudo -S docker compose -p communication -f /home/localization/Eurobot-2025-Localization/docker/testBot/OdomComm/docker/blue-compose.yml down" C-m
+
+# Attach to the session
+tmux attach-session -d
+
 # Delete the tmux session
 #tmux kill-session -t eurobot-script
 tmux kill-server
