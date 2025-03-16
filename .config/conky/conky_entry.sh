a243b0eaafb16b7949d80f8b6b9653fd9e9a7819
SeanChangX <scx@gapp.nthu.edu.tw>
2025-03-17 05:01:25 +0800

    Enhance volume control in conky_entry.sh for different Ubuntu versions

--- .config/conky/conky_entry.sh
+++ .config/conky/conky_entry.sh
@@ -5,9 +5,26 @@ DIR="/home/ditrobotics/.config/conky"
 conky -c $DIR/conky-esp-config/.conky_esp32 &
 $DIR/lean-conky-config/start-lcc.sh;
 
-# Set the volume to 95%
 sleep 3
-pactl set-sink-volume @DEFAULT_SINK@ 95%
+# Set the volume to 95%
+VOLUME_LEVEL="95%"
+UBUNTU_VERSION=$(lsb_release -r | awk '{print $2}')
+
+if [[ "$UBUNTU_VERSION" == "22.04" ]]; then
+    pactl set-sink-volume @DEFAULT_SINK@ "$VOLUME_LEVEL"
+elif [[ "$UBUNTU_VERSION" == "24.04" ]]; then
+    amixer set Master "$VOLUME_LEVEL"
+else
+    echo "Unrecognized Ubuntu version, trying to auto-detect available tools..."
+    if command -v pactl &> /dev/null; then
+        pactl set-sink-volume @DEFAULT_SINK@ "$VOLUME_LEVEL"
+    elif command -v amixer &> /dev/null; then
+        amixer set Master "$VOLUME_LEVEL"
+    else
+        echo "Unable to find volume control tool! Please check system installation."
+    fi
+fi
+
 # Boot-up sound effect
 sleep 1
 ffplay -nodisp -autoexit /home/ditrobotics/DIT-Scripts/.config/conky/conky-esp-config/welcome.mp3
