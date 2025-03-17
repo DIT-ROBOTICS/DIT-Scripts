2c951e85846337f2e79971a78ac945b32e02a77a
SeanChangX <scx@gapp.nthu.edu.tw>
2025-03-17 15:25:40 +0800

    Update remove.sh to use correct Docker project names for localization services

--- share/scripts/remove.sh
+++ share/scripts/remove.sh
@@ -4,8 +4,8 @@
 tmux new-session -d -s eurobot-kill-script
 
 # Remove localization
-tmux send-keys -t 0 "echo 'ditrobotics' | sudo -S docker compose -p communication -f /home/localization/Eurobot-2025-Localization/docker/testBot/OdomComm/docker/yellow-compose.yml down" C-m
-tmux send-keys -t 0 "echo 'ditrobotics' | sudo -S docker compose -p communication -f /home/localization/Eurobot-2025-Localization/docker/testBot/OdomComm/docker/blue-compose.yml down" C-m
+tmux send-keys -t 0 "echo 'ditrobotics' | sudo -S docker compose -p localization-yellow -f /home/localization/Eurobot-2025-Localization/docker/testBot/OdomComm/docker/yellow-compose.yml down" C-m
+tmux send-keys -t 0 "echo 'ditrobotics' | sudo -S docker compose -p localization-blue -f /home/localization/Eurobot-2025-Localization/docker/testBot/OdomComm/docker/blue-compose.yml down" C-m
 # Remove navigation
 tmux send-keys -t 0 "echo 'ditrobotics' | sudo -S docker compose -p navigation-run -f /home/navigation/Eurobot-2025-machine-ws/src/Eurobot-2025-Navigation2-envs/Navigation2-humble-deploy/docker-compose.yaml down" C-m
 # Remove main
