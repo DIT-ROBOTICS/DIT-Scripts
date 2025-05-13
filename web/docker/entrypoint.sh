0eeec6fd9c48dd9c93ce88fc5b2eafc21f1c06c9
SeanChangX <scx@gapp.nthu.edu.tw>
2025-05-14 07:59:43 +0800

    Update Eurobot-2025-Web submodule URL to use SSH and update submodule to latest commit; add rosbridge-server installation and launch in Docker setup

--- web/docker/entrypoint.sh
+++ web/docker/entrypoint.sh
@@ -6,6 +6,7 @@ if [ "$ROS_DISTRO" = 'noetic' ]; then
 elif [ "$ROS_DISTRO" = 'humble' ]; then
     source /opt/ros/humble/setup.bash
     ros2 launch foxglove_bridge foxglove_bridge_launch.xml &
+    ros2 launch rosbridge_server rosbridge_websocket_launch.xml &
     ros2 launch teleop_twist_joy teleop-launch.py joy_config:=xbox joy_vel:=cmd_vel_nav &
     ros2 run image_tools cam2image --ros-args --log-level WARN -p video_device:=/dev/video0;
 fi
