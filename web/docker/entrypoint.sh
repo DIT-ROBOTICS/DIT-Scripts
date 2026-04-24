34f473d18457fbbded1cfac189bd6581e2bbc5ed
SeanChangX <scx@gapp.nthu.edu.tw>
2026-04-25 02:48:15 +0800

    fix: add default parameters for rosbridge_server launch in entrypoint.sh

--- web/docker/entrypoint.sh
+++ web/docker/entrypoint.sh
@@ -10,7 +10,7 @@ elif [ "$ROS_DISTRO" = 'humble' ]; then
     services_glob="['/robot/startup/ready_signal', '/robot/startup/web_plan']"
     params_glob="['rival_inscribed_radius', 'dock_rival_radius', 'dock_rival_degree', 'pantry_aggressiveness', 'pantry_sensitivity', 'pantry_rival_sigma', 'pantry_rival_distance_threshold', 'collection_aggressiveness', 'collection_sensitivity', 'collection_rival_sigma', 'collection_rival_distance_threshold', 'flip_distance_threshold', 'cursor_tolerance', 'sima_start_time', 'plan_code']"
     actions_glob="['__blocked__']"
-    ros2 launch rosbridge_server rosbridge_websocket_launch.xml "topics_glob:=$topics_glob" "services_glob:=$services_glob" "params_glob:=$params_glob" "actions_glob:=$actions_glob" &
+    ros2 launch rosbridge_server rosbridge_websocket_launch.xml "topics_glob:=$topics_glob" "services_glob:=$services_glob" "params_glob:=$params_glob" "actions_glob:=$actions_glob" "default_call_service_timeout:=5.0" "call_services_in_new_thread:=true" "send_action_goals_in_new_thread:=true" &
     ros2 launch teleop_twist_joy teleop-launch.py joy_config:=xbox joy_vel:=cmd_vel_nav;
     # ros2 run image_tools cam2image --ros-args --log-level WARN -p video_device:=/dev/video0;
 fi
